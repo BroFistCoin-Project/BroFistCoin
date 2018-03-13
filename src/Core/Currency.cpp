d086148f2baa4f82f626e72f896cef216b41a924
Luís_ <luisfonseca720@gmail.com>
2018-03-13 23:16:44 +0000

    Update Currency.cpp

--- src/Core/Currency.cpp
+++ src/Core/Currency.cpp
@@ -82,9 +82,7 @@ Currency::Currency(bool is_testnet)
 	// Hard code coinbase tx in genesis block, because through generating tx use
 	// random, but genesis should be always
 	// the same
-	std::string genesis_coinbase_tx_hex =
-	    "010a01ff0001ffffffffffff0f029b2e4c0281c0b02e7c53291a94d1d0cbff8883f8024f"
-	    "5142ee494ffbbd08807121013c086a48c15fb637a96991bc6d53caf77068b5ba6eeb3c82357228c49790584a";
+	std::string genesis_coinbase_tx_hex = "";
 	BinaryArray miner_tx_blob;
 
 	bool r = from_hex(genesis_coinbase_tx_hex, miner_tx_blob);
